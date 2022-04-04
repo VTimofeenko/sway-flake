fd3b628c08a55004f76df0832e7cee6a0c29c410
Vladimir Timofeenko <id@vtimofeenko.com>
2022-04-03 20:44:05 -0700

    chore: split waybar into a separate module
    Closes 622cad5

--- sway.nix
+++ sway.nix
@@ -9,6 +9,9 @@ let
   my_modifier = "Mod4";
 in
 {
+  imports = [
+    ./modules/waybar.nix
+  ];
   # These options are taken from https://nix-community.github.io/home-manager/options.html
   wayland.windowManager.sway = {
     enable = true;
@@ -121,103 +124,6 @@ in
     };
   };
 
-  };
-
-  programs.waybar = {
-    enable = true;
-    systemd = {
-      enable = true;
-      /* # target = "sway-session.target"; # Available only on unstable as of Apr 3 2022 */
-    };
-    settings = [ {
-      /* Waybar config. */
-      /* Default is here: https://github.com/Alexays/Waybar/blob/master/resources/config */
-      layer = "bottom";
-      position = "top";
-      height = 30;
-      /* TODO: laptop-specific */
-      output = [
-        "eDP-1"
-      ];
-      modules-left = [ "sway/workspaces" "sway/mode" ];
-      modules-center = [ "sway/window" ];
-      modules-right = [ "idle_inhibitor" "pulseaudio" "network" "cpu" "memory" "temperature" "backlight" "sway/language" "battery" "clock" "tray" ];
-
-      modules = {
-        "sway/workspaces" = {
-          disable-scroll = true;
-          all-outputs = true;
-        };
-        "sway/mode"= {
-          "format" = "<span style=\"italic\">{}</span>";
-        };
-        "idle_inhibitor" = {
-          "format" = "{icon}";
-          "format-icons" = {
-            "activated" = "☕";
-            "deactivated" = "☕";
-          };
-        };
-        "temperature" = {
-          /* # TODO laptop-specific */
-          "thermal-zone" = 3;
-          "critical-threshold" = 80;
-          "format-critical" = "{temperatureC}°C {icon}";
-          "format" = "{temperatureC}°C {icon}";
-          "format-icons" = ["" "" ""];
-        };
-        "cpu" = {
-          "format" = "{usage}% ";
-          "tooltip" = false;
-        };
-        "memory" = {
-          "format" = "{}% ";
-        };
-        "backlight" = {
-          "format" = "{percent}% {icon}";
-          "format-icons" = ["" "" "" "" "" "" "" "" ""];
-        };
-        "battery" = {
-          "states" = {
-            /* "good" = 95 */
-            "warning" = 30;
-            "critical" = 15;
-          };
-          "format" = "{capacity}% {icon}";
-          "format-charging" = "{capacity}% ";
-          "format-plugged" = "{capacity}% ";
-          "format-alt" = "{time} {icon}";
-          "format-icons" = ["" "" "" "" ""];
-        };
-        "network" = {
-          "format-wifi" = "{essid} ({signalStrength}%) ";
-          "format-ethernet" = "{ipaddr}/{cidr} ";
-          "tooltip-format" = "{ifname} via {gwaddr} ";
-          "format-linked" = "{ifname} (No IP) ";
-          "format-disconnected" = "Disconnected ⚠";
-          "format-alt" = "{ifname} = {ipaddr}/{cidr}";
-        };
-        "pulseaudio" = {
-          "format" = "{volume}% {icon} {format_source}";
-          "format-bluetooth" = "{volume}% {icon} {format_source}";
-          "format-bluetooth-muted" = " {icon} {format_source}";
-          "format-muted" = " {format_source}";
-          "format-source" = "{volume}% ";
-          "format-source-muted" = "";
-          "format-icons" = {
-            "headphone" = "";
-            "hands-free" = "";
-            "headset" = "";
-            "phone" = "";
-            "portable" = "";
-            "car" = "";
-            "default" = ["" "" ""];
-          };
-          "on-click" = "pavucontrol";
-        };
-      };
-    } ];
-  };
   /* services.swayidle = {
     enable = true;
     events = [
