7b55c91af679d3dfa1d8327d2ee712d5b10cebee
Vladimir Timofeenko <id@vtimofeenko.com>
2022-04-23 10:39:11 -0700

    chore: nix-fmt

--- modules/waybar.nix
+++ modules/waybar.nix
@@ -18,14 +18,14 @@ in
       enable = true;
       /* # target = "sway-session.target"; # Available only on unstable as of Apr 3 2022 */
     };
-    settings = [ {
+    settings = [{
       /* Waybar config. */
       /* Default is here: https://github.com/Alexays/Waybar/blob/master/resources/config */
       layer = "bottom";
       position = "top";
       height = 30;
       /* TODO: laptop-specific */
-      output = [ "eDP-1" ];
+      /* output = [ "eDP-1" ]; */
       modules-left = [ "sway/workspaces" "sway/mode" ];
       modules-center = [ ];
       modules-right = [ "tray" "idle_inhibitor" "pulseaudio" "network" "temperature" "cpu" "sway/language" "battery" "clock" ];
@@ -40,7 +40,7 @@ in
           "format" = "{:%b %-d %R}";
           "tooltip" = false;
         };
-        "sway/mode"= {
+        "sway/mode" = {
           "format" = "<span style=\"italic\">{}</span>";
         };
         "idle_inhibitor" = {
@@ -56,7 +56,7 @@ in
           "critical-threshold" = 80;
           "format-critical" = mkSpan "{icon}" + " {temperatureC}°C";
           "format" = mkSpan "{icon}" + " {temperatureC}°C";
-          "format-icons" = ["" "" ""];
+          "format-icons" = [ "" "" "" ];
         };
         "cpu" = {
           "format" = mkSpan "" + " {usage}%";
@@ -72,7 +72,7 @@ in
           "format-charging" = mkSpan "{icon}" + " {capacity}% " + mkSpan "";
           "format-plugged" = mkSpan "{icon}" + " {capacity}% " + mkSpan "";
           "format-alt" = "{time} " + mkSpan "{icon}";
-          "format-icons" = ["" "" "" "" ""];
+          "format-icons" = [ "" "" "" "" "" ];
         };
         "network" = {
           "format-wifi" = mkSpan " " + " {essid}";
@@ -82,7 +82,7 @@ in
           "format-disconnected" = mkSpan "⚠" + " Disconnected";
         };
         "pulseaudio" = {
-          "format" =  mkSpan "{icon}" + " {volume}%";
+          "format" = mkSpan "{icon}" + " {volume}%";
           "format-bluetooth" = "{volume}% {icon} {format_source}";
           "format-bluetooth-muted" = " {icon} {format_source}";
           "format-muted" = "";
@@ -95,12 +95,12 @@ in
             "phone" = "";
             "portable" = "";
             "car" = "";
-            "default" = ["" "" ""];
+            "default" = [ "" "" "" ];
           };
           "on-click" = "pavucontrol";
         };
       };
-    } ];
+    }];
     style = ''
       ${builtins.readFile template}
       ${builtins.readFile ../assets/waybar.style.css}
