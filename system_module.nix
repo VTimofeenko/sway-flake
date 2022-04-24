e3a5c7f33d17978ac24391159a38f0fb160d6d1e
Vladimir Timofeenko <id@vtimofeenko.com>
2022-04-24 13:26:18 -0700

    feat: added fonts

--- system_module.nix
+++ system_module.nix
@@ -14,4 +14,22 @@
   programs.sway.enable = true;
   programs.dconf.enable = true;
   hardware.opengl.enable = true;
+  # fonts
+
+  fonts = {
+    fonts = with pkgs; [
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+      roboto
+      twitter-color-emoji
+      font-awesome
+    ];
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "JetBrainsMono Nerd Font" ];
+        sansSerif = [ "Roboto" ];
+        serif = [ "Roboto" ];
+        emoji = [ "Twitter Color Emoji" ];
+      };
+    };
+  };
 }
