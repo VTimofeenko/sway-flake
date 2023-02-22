0dc8e9b87d22a2e7787836935b1800cf4af303df
Vladimir Timofeenko <id@vtimofeenko.com>
2023-02-21 16:45:33 -0800

    [kitty] fix: remove tab bar if no tabs present

--- modules/kitty/default.nix
+++ modules/kitty/default.nix
@@ -13,7 +13,7 @@ in
       tab_bar_margin_width = 9;
       tab_bar_margin_height = "2 2";
       tab_bar_style = "separator";
-      tab_bar_min_tabs = 0;
+      tab_bar_min_tabs = 1;
       tab_bar_edge = "bottom";
       tab_title_template = "{fmt.fg.white} {title.split()[0]} ";
       active_tab_title_template = "{fmt.fg.black}{fmt.fg.blue} {title.split()[0]} ";
