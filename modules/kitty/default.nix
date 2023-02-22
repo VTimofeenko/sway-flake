2b98ea0133d248dd59ebeaa7883d27ea08094cff
Vladimir Timofeenko <id@vtimofeenko.com>
2023-02-21 16:49:40 -0800

    [kitty] chore: make tab bar appear as by default

--- modules/kitty/default.nix
+++ modules/kitty/default.nix
@@ -13,7 +13,7 @@ in
       tab_bar_margin_width = 9;
       tab_bar_margin_height = "2 2";
       tab_bar_style = "separator";
-      tab_bar_min_tabs = 1;
+      tab_bar_min_tabs = 2;
       tab_bar_edge = "bottom";
       tab_title_template = "{fmt.fg.white} {title.split()[0]} ";
       active_tab_title_template = "{fmt.fg.black}{fmt.bg.blue} {title.split()[0]} ";
